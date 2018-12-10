01507ab696f69cedcea14d281f7cee82e446182b
Felix Liu <felix.liu@hotmail.se>
2018-12-11 00:37:46 +0100

    Gjort om caesarkryptering för att fixa buggar.

--- src/Model/CaesarEncryption.java
+++ src/Model/CaesarEncryption.java
@@ -24,18 +24,10 @@ public class CaesarEncryption {
         for (int i = 0; i < message.length(); i++) {
             if (Character.isLetter(message.charAt(i))) {
                 //Code point for capital A if the current letter is uppercase or of lower case a otherwise.
-                int asciiShift = Character.isUpperCase(message.charAt(i)) ? (int) 'a' : (int) 'A';
+                int asciiShift = Character.isUpperCase(message.charAt(i)) ? (int) 'A' : (int) 'a';
                 //This will be a 0-25 value representing how many code points from 'a' or 'A' this character is.
                 int codePoint = (int) message.charAt(i) - asciiShift;
-                //Shift by the key
-                codePoint += key;
-                //Make sure the value is in range
-                codePoint %= keySize;
-                //Java modulus is the remainder, not the mathematical mod.
-                //We want the mathematical modulus, so no negative numbers.
-                if (codePoint < 0) {
-                    codePoint += keySize;
-                }
+                codePoint = ((codePoint + key + keySize) % keySize);
                 //Shift back the value to the characters in question.
                 codePoint += asciiShift;
 
